cd227f3626fee1fd1cb98bfe3afe28fa297a99d0
suntree99 <suntree99@ymail.com>
2020-10-09 15:56:03 +0700

    menambahkan folder 22_web_hosting_domain

---
+++ 11_php_dasar/22_web_hosting_domain/5_login.php
@@ -0,0 +1,96 @@
+<?php
+
+session_start();
+
+// menghubungkan dengan perintah dari file functions
+require 'functions.php';
+
+if ( isset($_COOKIE['id']) && isset($_COOKIE['key']) ) {
+  $id = $_COOKIE['id'];
+  $key = $_COOKIE['key'];
+
+  $result = mysqli_query($connectDB, "SELECT username FROM user WHERE id = $id");
+  $row = mysqli_fetch_assoc($result);
+
+  if ( $key === hash('sha256', $row['username']) ) {
+       $_SESSION['login'] = true;
+  }
+}
+
+if ( isset($_SESSION["login"]) ) {
+  header("Location: index.php");
+  exit;
+}
+
+// mengecek jika tombol login diklik
+if ( isset($_POST["login"]) ) {
+
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  $result = mysqli_query($connectDB, "SELECT * FROM user WHERE username = '$username'");
+
+  if ( mysqli_num_rows($result) === 1 ) {
+  
+    $row = mysqli_fetch_assoc($result);
+    if ( password_verify($password, $row["password"]) ) {
+
+      $_SESSION["login"] = true;
+
+      if ( isset($_POST['remember']) ) {
+
+        setcookie('id', $row['id'], time()+60);
+        setcookie('key', hash('sha256', $row['username']), time()+60);  
+      }
+
+      header("Location: index.php");
+      exit;
+    }
+  }
+
+  $error = true;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Halaman Login</title>
+</head>
+<body>
+  
+  <h1>Halaman Login</h1>
+  
+  <?php if ( isset($error) ) : ?>
+    <p style="color:red; font-style:italic">username / password salah</p>
+  <?php endif; ?>
+
+  <form action="" method="post">
+    <ul>
+      <li>
+        <label for="username">Username :</label>
+        <input type="text" name="username" id="username">
+      </li>
+      <li>
+        <label for="password">Password :</label>
+        <input type="password" name="password" id="password">
+      </li>
+      <li>
+        <input type="checkbox" name="remember" id="remember">
+        <label for="remember">Remember me</label>
+      </li>
+      <li>
+        <button type="submit" name="login">Login</button>
+      </li>
+    </ul>
+  </form>
+
+  <p>Belum memiliki akun? registrasi <a href="4_registrasi.php">disini.</a></p>
+  <p>Muroja'ah hafalan nama surat yuk <a href="hafal_surat/index_hafal_surat.html">disini.</a></p>
+  <p>Belajar HSI Dasar <a href="materi_hsi/index_materi_hsi_00.html">disini.</a></p>
+  <p>Belajar HSI Silsilah 1 <a href="materi_hsi/index_materi_hsi_01.html">disini.</a></p>
+  
+</body>
+</html>
