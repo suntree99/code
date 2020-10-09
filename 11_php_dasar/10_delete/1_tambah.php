5c6f32f3d165f3df41b97db05eea01ff6c0997a5
suntree99 <suntree99@ymail.com>
2020-10-09 15:49:12 +0700

    menambahkan folder 10_delete

---
+++ 11_php_dasar/10_delete/1_tambah.php
@@ -0,0 +1,72 @@
+<?php 
+
+// menghubungkan dengan perintah dari file functions.php
+require 'functions.php';
+
+// mengecek jika tombol submit sudah ditekan
+if ( isset($_POST["submit"]) ) {
+
+  // mengecek apakah data berhasil ditambahkan atau tidak
+  if ( tambah($_POST) > 0 ) {
+    echo "
+      <script>
+        alert('Data BERHASIL ditambahkan');
+        document.location.href = 'index.php';
+      </script>";
+  } else {
+    echo "
+      <script>
+        alert('Data GAGAL ditambahkan');
+        document.location.href = 'index.php';
+      </script>";
+  }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Tambah Data Karyawan</title>
+</head>
+<body>
+
+  <h1>Tambah Data Karyawan</h1>
+
+  <form action="" method="post">
+    <ul>
+      <li>
+        <label for="nik">NIK :</label>
+        <input type="text" name="nik" id="nik" required>
+      </li>
+      <li>
+        <label for="nama">Nama :</label>
+        <input type="text" name="nama" id="nama">
+      </li>
+      <li>
+        <label for="usia">Usia :</label>
+        <input type="text" name="usia" id="usia">
+      </li>
+      <li>
+        <label for="email">Email :</label>
+        <input type="text" name="email" id="email">
+      </li>
+      <li>
+        <label for="gambar">Gambar :</label>
+        <input type="text" name="gambar" id="gambar">
+      </li>
+      <li>
+        <button type="submit" name="submit">
+          Tambah Data!
+        </button>
+      </li>
+    </ul>
+
+  </form>
+
+  <a href="index.php">Kembali ke Halaman Data Karyawan</a>
+
+</body>
+</html>
---
+++ 11_php_dasar/10_delete/functions.php
@@ -0,0 +1,44 @@
+<?php 
+
+// koneksi ke database
+$connectDB = mysqli_connect("localhost", "root", "", "phpdasar");
+
+// function query - mempersingkat perintah query
+function query($query) {
+  global $connectDB;
+  $result = mysqli_query($connectDB, $query);
+  $rows = [];
+  while ( $row = mysqli_fetch_assoc($result) ) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
+
+// function tambah
+function tambah($data) {
+  global $connectDB;
+  $nama = htmlspecialchars($data["nama"]);
+  $nik = htmlspecialchars($data["nik"]);
+  $usia = htmlspecialchars($data["usia"]);
+  $email = htmlspecialchars($data["email"]);
+  $gambar = htmlspecialchars($data["gambar"]);
+
+  $query = "INSERT INTO Karyawan VALUES
+            ('', '$nama', '$nik', '$usia', '$email', '$gambar')";
+
+  mysqli_query($connectDB, $query);
+
+  return mysqli_affected_rows($connectDB);
+}
+
+// function hapus
+function hapus($id) {
+  global $connectDB;
+
+  // mengeksekusi query delete data, jangan lupa tambahkan kekhususan (WHERE)
+  mysqli_query($connectDB, "DELETE FROM karyawan WHERE id = $id");
+
+  return mysqli_affected_rows($connectDB);
+}
+
+?>
