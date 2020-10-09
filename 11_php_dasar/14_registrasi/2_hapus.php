70a9ef80e29fe51b5de838002da1061efce5b496
suntree99 <suntree99@ymail.com>
2020-10-09 15:51:48 +0700

    menambahkan folder 14_registrasi

---
+++ 11_php_dasar/14_registrasi/2_hapus.php
@@ -0,0 +1,25 @@
+<?php 
+
+// menghubungkan dengan perintah dari file functions.php
+require 'functions.php';
+
+// menangkap $_GET dari halaman index.php
+$id = $_GET["id"];
+
+// mengecek apakah data berhasil dihapus atau tidak
+if ( hapus($id) > 0 ) {
+  echo "
+  <script>
+    alert('Data BERHASIL dihapus');
+    document.location.href = 'index.php';
+  </script>";
+} else {
+  echo "
+  <script>
+    alert('Data GAGAL dihapus');
+    document.location.href = 'index.php';
+  </script>";
+
+}
+
+?>
