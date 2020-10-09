c13e9fd5fe27eb2799f35e9d3d1b0e8b5b20a7bc
suntree99 <suntree99@ymail.com>
2020-10-09 15:50:09 +0700

    menambahkan folder 12_read_search

---
+++ 11_php_dasar/12_read_search/3_ubah.php
@@ -0,0 +1,79 @@
+<?php 
+
+// menghubungkan dengan perintah dari file functions.php
+require 'functions.php';
+
+// menangkap $_GET dari halaman index.php
+$id = $_GET["id"];
+
+// melakukan query data berdasarkan id
+$kry = query("SELECT * FROM Karyawan where id = $id")[0]; // perlu ditambahkan [0] karena hasil query() saja adalah berupa array numerik [0] yang berisi array data satu row 
+
+// mengecek jika tombol submit sudah ditekan
+if ( isset($_POST["submit"]) ) {
+
+  // mengecek apakah data berhasil diubah atau tidak
+  if ( ubah($_POST) > 0 ) {
+    echo "
+      <script>
+        alert('Data BERHASIL diubah');
+        document.location.href = 'index.php';
+      </script>";
+  } else {
+    echo "
+      <script>
+        alert('Data GAGAL diubah');
+        document.location.href = 'index.php';
+      </script>";
+  }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Ubah Data Karyawan</title>
+</head>
+<body>
+
+  <h1>Ubah Data Karyawan</h1>
+
+  <form action="" method="post">
+    <input type="hidden" name="id" value="<?= $kry["id"]; ?>">
+    <ul>
+      <li>
+        <label for="nik">NIK :</label>
+        <input type="text" name="nik" id="nik" required value="<?= $kry["nik"]; ?>">
+      </li>
+      <li>
+        <label for="nama">Nama :</label>
+        <input type="text" name="nama" id="nama" value="<?= $kry["nama"]; ?>">
+      </li>
+      <li>
+        <label for="usia">Usia :</label>
+        <input type="text" name="usia" id="usia" value="<?= $kry["usia"]; ?>">
+      </li>
+      <li>
+        <label for="email">Email :</label>
+        <input type="text" name="email" id="email" value="<?= $kry["email"]; ?>">
+      </li>
+      <li>
+        <label for="gambar">Gambar :</label>
+        <input type="text" name="gambar" id="gambar" value="<?= $kry["gambar"]; ?>">
+      </li>
+      <li>
+        <button type="submit" name="submit">
+          Ubah Data!
+        </button>
+      </li>
+    </ul>
+
+  </form>
+
+  <a href="index.php">Kembali ke Halaman Data Karyawan</a>
+
+</body>
+</html>
---
+++ 11_php_dasar/12_read_search/functions.php
@@ -0,0 +1,86 @@
+<?php 
+
+// koneksi ke database
+$connectDB = mysqli_connect("localhost", "root", "", "phpdasar");
+
+// function query - mempersingkat perintah query
+function query($query) {
+  global $connectDB;
+  $result = mysqli_query($connectDB, $query);
+  $rows = [];
+  while ( $row = mysqli_fetch_assoc($result) ) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
+
+// function tambah
+function tambah($data) {
+  global $connectDB;
+  $nama = htmlspecialchars($data["nama"]);
+  $nik = htmlspecialchars($data["nik"]);
+  $usia = htmlspecialchars($data["usia"]);
+  $email = htmlspecialchars($data["email"]);
+  $gambar = htmlspecialchars($data["gambar"]);
+
+  $query = "INSERT INTO Karyawan VALUES
+            ('', '$nama', '$nik', '$usia', '$email', '$gambar')";
+
+  mysqli_query($connectDB, $query);
+
+  return mysqli_affected_rows($connectDB);
+}
+
+// function hapus
+function hapus($id) {
+  global $connectDB;
+
+  mysqli_query($connectDB, "DELETE FROM karyawan WHERE id = $id");
+
+  return mysqli_affected_rows($connectDB);
+}
+
+// function ubah
+function ubah($data) {
+  global $connectDB;
+  $id = $data["id"];
+  $nama = htmlspecialchars($data["nama"]);
+  $nik = htmlspecialchars($data["nik"]);
+  $usia = htmlspecialchars($data["usia"]);
+  $email = htmlspecialchars($data["email"]);
+  $gambar = htmlspecialchars($data["gambar"]);
+
+  // query update data
+  $query = "UPDATE Karyawan SET
+            nama = '$nama',
+            nik = '$nik',
+            usia = '$usia',
+            email = '$email',
+            gambar = '$gambar'
+            WHERE
+            id = $id
+           ";
+
+  mysqli_query($connectDB, $query);
+
+  return mysqli_affected_rows($connectDB);
+}
+
+// function cari
+function cari($keyword) { // parameter $keyword disisi oleh $_POST["keyword"] dari form search
+  $query = "SELECT * FROM karyawan WHERE
+            nama LIKE '%$keyword%' OR
+            nik LIKE '%$keyword%' OR
+            usia LIKE '%$keyword%' OR
+            email LIKE '%$keyword%'
+           ";
+
+  return query($query);
+}
+
+// LIKE digunakan agar pencarian dilakukan dengan kata yang mengandung keyword
+  // jika menggunakan sama dengan (=) maka keyword harus sama PERSIS dengan data yang dicari
+// % berfungsi agar karakter apapun sebelum/setelah keyword tidak berpengaruh
+// tambahkan kutip ('') karena variabel harus berupa string
+
+?>
