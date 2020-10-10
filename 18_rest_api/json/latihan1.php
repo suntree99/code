721a75b101b9ec37e208d63f9c26b0adca7bc232
suntree99 <suntree99@ymail.com>
2020-10-10 07:29:08 +0700

    membuat object json dengan php

---
+++ 18_rest_api/json/latihan1.php
@@ -0,0 +1,24 @@
+<?php 
+
+$mahasiswa = [
+    [   
+        "nama" => "Budi Darmawan",
+        "nim" => 13608051,
+        "email" => "suntree99@ymail.com"    
+    ],
+    [
+        "nama" => "Budi Darmawan",
+        "nim" => 13608051,
+        "email" => "suntree99@ymail.com"
+    ],
+    [
+        "nama" => "Budi Darmawan",
+        "nim" => 13608051,
+        "email" => "suntree99@ymail.com"
+    ]
+];
+
+$data = json_encode($mahasiswa);
+echo ($data);
+
+?>
